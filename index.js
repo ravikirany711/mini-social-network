b20414922779b1b3dc67d9e40d98656656c92f89
ravikirany711 <ravikirany711@gmail.com>
2019-03-15 19:47:04 +0530

    basic express routes setup done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const user = require('./routes/api/user')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
+
 const app = express()
 
 //connecting to database
@@ -11,9 +16,19 @@ mongoose.connect(db)
 
 
 
+
+
 app.get('/', (req, res) => {
     res.send('express is up and running')
 })
+
+
+//Use Routes
+app.use('/api/user', user)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+
+
 
 app.listen(process.env.PORT || 5000, (req, res) => {
     console.log('server is up and running on port 5000,go ahead')
