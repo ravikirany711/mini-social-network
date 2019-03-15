4ac34612dc52da28ef4f9ef50e88116291ae7d9c
ravikirany711 <ravikirany711@gmail.com>
2019-03-15 17:27:59 +0530

    database connected using mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ravi123:ravi123@dev-connector-last-try-0bapm.mongodb.net/test?retryWrites=true'
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+//connecting to database
+const db = require('./config/keys').mongoURI
+mongoose.connect(db)
+    .then(() => console.log('Mongodb connected'))
+    .catch((err) => console.log(err))
+
+
 
 app.get('/', (req, res) => {
     res.send('express is up and running')
